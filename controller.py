c86e7489312f49441c746164646f7a629aed653c
sphil5 <195391783+sphil5@users.noreply.github.com>
2025-10-24 12:43:27 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,1 +1,10 @@
+class Item:
+  def __innit__ (self, name):
+    self.name(name) = "milk"
+
+  def __innit__ (self, price):
+    self.price(price) = 4
+
+  def __innit__ (self, count):
+    self.count(count) = 10
 
